3f70cdd12891de82bc18af729aa555c43603824a
Bogdan <ortbg1988@gmail.com>
2021-10-11 14:23:33 +0300

    [add] add task-2.js solving

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const makeIngredientListItem = (ingredients = []) => {
+    const listEl = document.querySelector("ul#ingredients");
+  ingredients.forEach(ingredien => {
+    const itemEl = document.createElement('li');
+    itemEl.textContent = ingredien;
+    itemEl.classList.add('item')
+    listEl.append(itemEl);
+  })
+}
+makeIngredientListItem(ingredients);
+
+
+
+
