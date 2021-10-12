3e687db1b10adaef7e48c49b979cde2011b427e9
Bogdan <ortbg1988@gmail.com>
2021-10-12 10:45:23 +0300

    [add] add task-04.js solving

---
+++ js/task-04.js
@@ -0,0 +1,9 @@
+
+let counterValue = 0;
+
+const valueEl = document.querySelector('span#value');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+
+incrementBtn.addEventListener('click', () => { counterValue += 1; valueEl.textContent = counterValue; })
+decrementBtn.addEventListener('click', () => { counterValue -= 1; valueEl.textContent = counterValue; })
