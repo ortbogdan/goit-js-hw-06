39df773f83349642e59d8e99072fe163063f23eb
Bogdan <ortbg1988@gmail.com>
2021-10-13 13:17:17 +0300

    [add] add task-08 solving

--- js/task-08.js
+++ js/task-08.js
@@ -14,21 +14,38 @@
 // Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
 // Під час відправлення форми сторінка не повинна перезавантажуватися.
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
-// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
+// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, 
+// а значення поля - значенням властивості.Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
 
-// const refs = {
-//     form: document.querySelector('form.login-form'),
-//     email: document.querySelector('input[type ="email"]'),
-//     password: document.querySelector('input[type ="password"]'),
-// }
-console.log("formRef");
-const formRef = document.querySelector('form.login-form');
 
-console.log('vcvxcvxv')
+const formRef = document.querySelector('form.login-form');
+const emailRef = document.querySelector('input[type="email"]');
+const passwordRef = document.querySelector('input[type="password"]');
+
+
+
+
+
 const onTargetSendForm = event => {
-    event.praventDefault();
-    console.log('js')
+    
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    
+    const formData = {
+        email,
+        password,
+    }
+
+    if (!password && !email) {
+        alert('Введіть дані!')
+    }
+    else console.log(formData);
+    formRef.reset();
+   
 }
 
 formRef.addEventListener('submit', onTargetSendForm)
