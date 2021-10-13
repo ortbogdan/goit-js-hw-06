ba9b098fa75f07b1daebd6674f99b88f66b99c4d
Bogdan <ortbg1988@gmail.com>
2021-10-13 14:02:05 +0300

    [add] add tesk-09 solving

--- js/task-08.js
+++ js/task-08.js
@@ -1,31 +1,7 @@
-// Напиши скрипт управління формою логіна.
-
-// <form class="login-form">
-//   <label>
-//     Email
-//     <input type="email" name="email" />
-//   </label>
-//   <label>
-//     Password
-//     <input type="password" name="password" />
-//   </label>
-//   <button type="submit">Login</button>
-// </form>
-// Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
-// Під час відправлення форми сторінка не повинна перезавантажуватися.
-// Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
-// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, 
-// а значення поля - значенням властивості.Для доступу до елементів форми використовуй властивість elements.
-// Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
-
 
 const formRef = document.querySelector('form.login-form');
 const emailRef = document.querySelector('input[type="email"]');
 const passwordRef = document.querySelector('input[type="password"]');
-
-
-
-
 
 const onTargetSendForm = event => {
     
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+
+const buttonRef = document.querySelector('button.change-color');
+const bodyRef = document.querySelector('body');
+const spanRef = document.querySelector('span.color');
+
+buttonRef.addEventListener('click', onClickChangeColor)
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function onClickChangeColor(event) {
+  const color = getRandomHexColor() 
+  spanRef.textContent = color;
+  bodyRef.style.backgroundColor = color; 
 }
