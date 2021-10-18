0b9bce40dfd9a9e0fc7ad3ed785038ae07e9f646
Bogdan <ortbg1988@gmail.com>
2021-10-18 16:02:08 +0300

    [add] add changes to task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -4,16 +4,16 @@ const boxesRef = document.querySelector('div#boxes');
 const createBtnRef = document.querySelector('button[data-create]');
 const destroyBtnRef = document.querySelector('button[data-destroy]')
 
-
+// const [input, create, destroy] = document.querySelector("#controls").children;
 createBtnRef.addEventListener('click', onClickAddBoxes)
 destroyBtnRef.addEventListener('click', onClickDestroyBoxes)
-
+let size = 30;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 function createBoxes(amount) {
-  let size = 30;
+  
   const boxes = [];
   for (let i = 1; i <= amount; i += 1) {
     const box = document.createElement('div');
@@ -33,5 +33,8 @@ function onClickAddBoxes(event) {
   createBoxes(amount)
 }
 function onClickDestroyBoxes(event) {
+  size = 30;
   boxesRef.innerHTML = '';
 }
+
+
